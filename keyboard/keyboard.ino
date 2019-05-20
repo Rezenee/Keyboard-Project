4358aff276d4c8878f768f21a1553481d5cdd323
Zane Rickert <02zane.rickert@gulllakecs.org>
2019-05-20 09:48:13 -0400

    make keypad numpad

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -2,38 +2,50 @@
  Created and Designed by Zane Rickert and Tyson Watson
   This program is going to be a 16% keyboard that
   can send basic inputs to the computer.
-  Took some of the code from https://www.pjrc.com/teensy/td_libs_Keypad.html
-AIMMMIICC ` ``  `AAEEEEEAAEICCDDDLGKBBBBBBCCKKKKKAEIAAAAAAAAAAAA
-  MMMIIIIMCGGKKOOPLHDAEEIIIMMMKKDDDHHLPKKKKKGMEIACCDAAAAEEEEEEEEEEEEEEIIIIMIAAAAAAAEEEMMMMMIMMMMEEMEMEMMEEEMMEEMMMPPPLLP
+  Took some of the code from https://www.pjrc.com/teensy/td_libs_Keypad.html123123456789
+                7741  2587
+  
  */
 
 
 #include "Key.h"
 #include <Keypad.h>
 #include <Keyboard.h>
-
+int change_layer = 0;
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
-  {'A','E','I','M'},
-  {'B','F','J', 'N'},
-  {'C','G','K', 193},
-  {'D','H','L', ' '}                      
+  {'7','4', '1',179},
+  {'8','5', '2', 216},
+  {'9','6', '3', 215},
+  {178,176, 218, 217}                      
 };
 byte rowPins[ROWS] = {6,7,8,9}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {2, 3, 4, 5}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
-  Keyboard.write('a');
 }
 
-void loop(){
+void loop()
+{
   char key = keypad.getKey();
-
+  if (key == 179) 
+  {
+    char keys[ROWS][COLS] = {
+  {'7777','4', '1',179},
+  {'8','5', '2', 216},
+  {'9','6', '3', 215},
+  {178,176, 218, 217}                      
+};
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+  }
   if (key != NO_KEY){
     Keyboard.write(key);
   }
+  
 }
+
